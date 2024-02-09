fba271e8c4165b6cf9ffb42c99186c50c12d222a
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-09 13:24:19 -0500

    add appendChild li to ul

--- domWork2.js
+++ domWork2.js
@@ -1,1 +1,14 @@
-console.log("hthere");
+var selectTheInputField = document.querySelector("input");
+var selectTheButton = document.querySelector("button");
+var selectTheUnorderedList = document.querySelector("ul");
+var createListItem = document.createElement("li");
+var inputValue = selectTheInputField.value;
+
+
+let addToUnorderedList = function(){
+  console.log("This is the value of input: "+ selectTheInputField.value);
+  selectTheUnorderedList.appendChild(createListItem);
+  // return selectTheInputField.value;s
+};
+
+selectTheButton.addEventListener("click", addToUnorderedList);
