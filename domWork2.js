f172a0451200ed871daf9f63f2b99ab99446d8c1
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-10 00:17:45 -0500

    add field clearing to input field

--- domWork2.js
+++ domWork2.js
@@ -10,38 +10,8 @@ let addToUnorderedList = function(){
   createListItem.appendChild(document.createTextNode(selectTheInputField.value));
   console.log("This is the value of input: "+ selectTheInputField.value);
   selectTheUnorderedList.appendChild(createListItem);
+  selectTheInputField.value = "";
 };
 
 selectTheButton.addEventListener("click", addToUnorderedList);
 
-// ------------------------------------------------------------
-// var button = document.getElementById("enterButton");
-// var textInput = document.getElementById("userInput");
-// var unorderedList = document.querySelector("ul");
-
-// let addToList = function(){
-//     let newLi = document.createElement("li");
-//     newLi.appendChild(document.createTextNode(textInput.value));
-//     unorderedList.appendChild(newLi);
-//     textInput.value = "";
-//   };
-
-// let addToEndOfListKeypress = function(onkeydown){
-//   if(onkeydown.key === "Enter" && textInput.value.length > 0){
-//     console.log(onkeydown.key + " key was pressed.");
-//     console.log(textInput.value);
-//     addToList();
-//     }
-//   };
-
-
-// let addToEndOfListButton = function(){
-//   if(textInput.value.length > 0){
-//     console.log(textInput.value);
-//     addToList();
-//   }  
-// };
-
-// textInput.addEventListener("keydown", addToEndOfListKeypress);
-
-// button.addEventListener("click", addToEndOfListButton);
