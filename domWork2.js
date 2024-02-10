59074caa580789a6f70a4cfea26e55109c0bea6f
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-09 19:58:09 -0500

    add addToUnorderedList function, updated CSS

--- domWork2.js
+++ domWork2.js
@@ -1,14 +1,47 @@
 var selectTheInputField = document.querySelector("input");
 var selectTheButton = document.querySelector("button");
 var selectTheUnorderedList = document.querySelector("ul");
-var createListItem = document.createElement("li");
-var inputValue = selectTheInputField.value;
+
+
 
 
 let addToUnorderedList = function(){
+  let createListItem = document.createElement("li");
+  createListItem.appendChild(document.createTextNode(selectTheInputField.value));
   console.log("This is the value of input: "+ selectTheInputField.value);
   selectTheUnorderedList.appendChild(createListItem);
-  // return selectTheInputField.value;s
 };
 
 selectTheButton.addEventListener("click", addToUnorderedList);
+
+// ------------------------------------------------------------
+// var button = document.getElementById("enterButton");
+// var textInput = document.getElementById("userInput");
+// var unorderedList = document.querySelector("ul");
+
+// let addToList = function(){
+//     let newLi = document.createElement("li");
+//     newLi.appendChild(document.createTextNode(textInput.value));
+//     unorderedList.appendChild(newLi);
+//     textInput.value = "";
+//   };
+
+// let addToEndOfListKeypress = function(onkeydown){
+//   if(onkeydown.key === "Enter" && textInput.value.length > 0){
+//     console.log(onkeydown.key + " key was pressed.");
+//     console.log(textInput.value);
+//     addToList();
+//     }
+//   };
+
+
+// let addToEndOfListButton = function(){
+//   if(textInput.value.length > 0){
+//     console.log(textInput.value);
+//     addToList();
+//   }  
+// };
+
+// textInput.addEventListener("keydown", addToEndOfListKeypress);
+
+// button.addEventListener("click", addToEndOfListButton);
