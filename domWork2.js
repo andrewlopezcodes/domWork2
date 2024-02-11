e77d23482a2e35da37ab63cac8bc37b87748f8d0
Andrew Lopez <andrewlopezcodes@gmail.com>
2024-02-10 23:46:27 -0500

    add randomNumberGenerator function, create random id values for li items

--- domWork2.js
+++ domWork2.js
@@ -6,6 +6,7 @@ var selectListItems = document.querySelectorAll("li");
 
 let addToUnorderedList = function(){
   let createButton;
+  let idNumber;
   let createListItem = document.createElement("li");
   
   let addDeleteButton = function(){
@@ -13,11 +14,22 @@ let addToUnorderedList = function(){
     createButton.setAttribute("class", "delete");
     createButton.innerText = "Delete";
   };
-    
+
+  let randomNumberGenerator = function(){
+    let randomNumber = Math.random();
+    let madeToString = randomNumber.toString();
+    idNumber = madeToString.slice(3, 9);
+    console.log("The new list item has an ID# of " + idNumber);
+  };
+  
+
+
   createListItem.appendChild(document.createTextNode(selectTheInputField.value + "    "));
   console.log("The new list item has an input value of: " + selectTheInputField.value);
   selectTheUnorderedList.appendChild(createListItem);
   addDeleteButton();
+  randomNumberGenerator();
+  createListItem.setAttribute("id", idNumber);
   createListItem.insertAdjacentElement('beforeend', createButton);
   //delete button functionality
   createButton.addEventListener("click", function(){
